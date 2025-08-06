85d27aa2974f930513d734710605097d5b77f70b
ahmed0thman <ahmedneo117@gmail.com>
2025-08-07 00:51:39 +0300

    done

--- app/(root)/product/[id]/page.tsx
+++ app/(root)/product/[id]/page.tsx
@@ -4,6 +4,7 @@ import ProductSwiper from "@/components/custom/product/productSwiper";
 import ProductMainInfo from "@/components/custom/product/productMainInfo";
 import ProductRatingsComments from "@/components/custom/product/productRatingsComments";
 import { getProduct } from "@/lib/api/apiProducts";
+import { Product } from "@/types";
 
 interface ProductPageProps {
   params: Promise<{ id: string }>;
@@ -12,10 +13,17 @@ interface ProductPageProps {
 const ProductPage = async ({ params }: ProductPageProps) => {
   const { id: productId } = await params;
   const product = await getProduct(productId);
+  if (!product) {
+    return (
+      <div className="flex-center h-screen">
+        <h1 className="text-2xl font-bold">Product not found</h1>
+      </div>
+    );
+  }
   return (
     <section className="space-y-12 pt-6 px-4 sm:px-8">
       {/* Main Info */}
-      <ProductMainInfo product={product} />
+      <ProductMainInfo product={product as Product} />
 
       {/* Ratings and Comments */}
       <ProductRatingsComments product={product} />
