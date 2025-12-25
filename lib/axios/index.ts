7cf395b7797cdbaceaa2a06584a8afaef79d1dbf
ahmed0thman <ahmedneo117@gmail.com>
2025-12-25 20:51:14 +0000

    test

--- lib/axios/index.ts
+++ lib/axios/index.ts
@@ -14,6 +14,7 @@ const getLanguage = async () => {
 
 export const api = axios.create({
   baseURL: API_URL,
+  timeout: 1000, // 10 seconds
   headers: {
     "Content-Type": "application/json",
   },
